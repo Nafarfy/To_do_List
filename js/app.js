5e7336f804bd4554ed1aed1540683aaef011e037
Your NameNafarfy <you@example.comthatsickdatsik@gmail.com>
2021-01-14 16:11:12 +0200

    logic separated

--- js/app.js
+++ js/app.js
@@ -2,65 +2,23 @@ const taskInput = document.querySelector(".add-task-input");
 const addBtn = document.querySelector(".add-task-btn");
 const taskList = document.querySelector(".task-list");
 
-class ToDoList {
-  tasks = [];
-}
+const toDoList = new ToDoList();
 
-ToDoList.prototype.renderTasks = function () {
-  taskList.innerHTML = this.tasks
+const renderTasks = function () {
+  taskList.innerHTML = toDoList.tasks
     .map((task) => {
-      return this.createTask(task);
+      return createTask(task);
     })
     .join("");
 };
 
-ToDoList.prototype.addTask = function (taskName) {
-  const task = {
-    id: this.tasks.length + 1,
-    taskName,
-    isDone: false,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  this.tasks = [...this.tasks, task];
-  this.renderTasks();
-
-  return task;
-};
-
-ToDoList.prototype.deleteTask = function (taskId) {
-  this.tasks = this.tasks.filter((task) => task.id !== taskId);
-  this.renderTasks();
-};
-
-ToDoList.prototype.toggleTaskComplete = function (taskId) {
-  return (this.tasks = this.tasks.map((task) => {
-    if (task.id === taskId) {
-      return {
-        ...task,
-        isDone: !task.isDone,
-        updatedAt: new Date(),
-      };
-    }
-
-    return task;
-  }));
-};
-
-ToDoList.prototype.createTask = function (task) {
+const createTask = function (task) {
   return `
   <li class="list-group-item d-list-item mb-1">${task.taskName}
     <button class="delete-btn float-end"><span>X</span></button>
   </li>
   `;
 };
-
-ToDoList.prototype.getTasks = function () {
-  return this.tasks;
-};
-
-const toDoList = new ToDoList();
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -70,5 +28,6 @@ addBtn.addEventListener("click", (e) => {
   }
 
   toDoList.addTask(taskInput.value);
+  renderTasks();
   taskInput.value = "";
 });
---
+++ js/library.js
@@ -0,0 +1,39 @@
+class ToDoList {
+  tasks = [];
+}
+
+ToDoList.prototype.addTask = function (taskName) {
+  const task = {
+    id: this.tasks.length + 1,
+    taskName,
+    isDone: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  this.tasks = [...this.tasks, task];
+
+  return task;
+};
+
+ToDoList.prototype.deleteTask = function (taskId) {
+  this.tasks = this.tasks.filter((task) => task.id !== taskId);
+};
+
+ToDoList.prototype.toggleTaskComplete = function (taskId) {
+  return (this.tasks = this.tasks.map((task) => {
+    if (task.id === taskId) {
+      return {
+        ...task,
+        isDone: !task.isDone,
+        updatedAt: new Date(),
+      };
+    }
+
+    return task;
+  }));
+};
+
+ToDoList.prototype.getTasks = function () {
+  return this.tasks;
+};
